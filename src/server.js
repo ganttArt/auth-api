b88d8fb1cf17ff3cd65b0b4e543d4197a2d658f2
Chris Gantt <gantt.art@gmail.com>
2021-04-28 13:19:46 -1000

    Combined two server.js files from starter code into new server.js file.

---
+++ src/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const notFoundHandler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
+const logger = require('./middleware/logger.js');
+
+// App Routes
+const authRoutes = require('./auth/routes.js');
+const v1Routes = require('./routes/v1.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+
+// Routes
+app.use(authRoutes);
+app.use('/api/v1', v1Routes);
+
+// Catch Alls
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error("Missing Port"); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
